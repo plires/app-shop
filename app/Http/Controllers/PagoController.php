b85dc99d144a7f17d234b4dd709e10433319decc
Pablo Lires <pablolires@gmail.com>
2018-08-20 23:16:52 -0300

    mercadopago test 5

--- app/Http/Controllers/PagoController.php
+++ app/Http/Controllers/PagoController.php
@@ -123,61 +123,8 @@ class PagoController extends Controller
 
 	public function ipnNotification(Request $request)
 	{
-	    $mp = new MP (env('2664232087964621'), env('hrG1pYSukzesHP4UrYlUU7FulvzgpTP4'));
-
-	    if ( ! isset($_GET["id"], $_GET["topic"]) || ! ctype_digit($_GET["id"])) {
-	        abort(404);
-	    }
-
-	    // Get the payment and the corresponding merchant_order reported by the IPN.
-	    if ($_GET["topic"] == 'payment') {
-	        $payment_info = $mp->get("/collections/notifications/" . $_GET["id"]);
-	        $merchant_order_info = $mp->get("/merchant_orders/" . $payment_info["response"]["collection"]["merchant_order_id"]);
-	        
-	        // Get the merchant_order reported by the IPN.
-
-	        // get order and link the notification id
-	        $external_reference_id = $merchant_order_info["response"]["external_reference"];
-	       //here you must clear unnecessary data in external reference
-	       print_r("algo hace aca.");
-	       // get order
-	        $order = Order::findOrFail($external_reference_id);
-	        // link notification id
-	        $order->mp_notification_id = $_GET["id"];
-
-	        if ($merchant_order_info["status"] == 200) {
-	            // If the payment's transaction amount is equal (or bigger) than the merchant_order's amount you can release your items
-	            
-	            $paid_amount = 0;
-
-	            foreach ($merchant_order_info["response"]["payments"] as $payment) {
-	                $order->status = $payment['status'];
-	                if ($payment['status'] == 'approved') {
-	                    $paid_amount += $payment['transaction_amount'];
-	                }
-	            }
-
-	            if ($paid_amount >= $merchant_order_info["response"]["total_amount"]) {
-	                if (count($merchant_order_info["response"]["shipments"]) > 0) { 
-	                    
-	                    // The merchant_order has shipments
-	                    if ($merchant_order_info["response"]["shipments"][0]["status"] == "ready_to_ship") {
-	                        print_r("Totally paid. Print the label and release your item.");
-	                    }
-	                } else {
-	                    // The merchant_order don't has any shipments
-	                    print_r("Totally paid. Release your item.");
-	                }
-	            } else {
-	                print_r("Not paid yet. Do not release your item.");
-	            }
-	        }
-
-	        $order->save();
-
-	    }
-
-	    return response('OK', 201);
+	    return header("HTTP/1.1 200 OK");
+ 			return \Response::json(['HTTP/1.1 200 OK'], 200);
 	}
 
 }
